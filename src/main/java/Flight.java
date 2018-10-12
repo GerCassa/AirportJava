e95f3255a458a37b0bb18ee1bbb4e6f3e8961e9f
GerCassa <gercassa@gmail.com>
2018-10-12 14:40:51 +0100

    added and passed tests on flight

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,25 @@
 public class Flight {
+
+    private PlaneType plane;
+    private int flightNumber;
+    private String destination;
+
+    public Flight(PlaneType plane, int flightNumber, String destination) {
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+    }
+
+
+    public PlaneType getPlane() {
+        return plane;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+    Plane plane;
+
+    @Before
+    public void before() {
+        flight = new Flight(PlaneType.AirbusA34, 1, "Buenos Aires");
+    }
+
+    @Test
+    public void hasPlane() {
+        assertEquals(PlaneType.AirbusA34, flight.getPlane());
+    }
+
+    @Test
+    public void getFlightNumber() {
+        assertEquals(1, flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("Buenos Aires", flight.getDestination());
+    }
+
 }
